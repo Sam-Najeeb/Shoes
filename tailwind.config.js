42821d2796ca324f27c72b5a8c21445160b988aa
Sm-Najeeb <samllm456@gmaill.com>
2021-06-16 10:21:29 +0100

    test website

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        Primary: {
+          100: "#faf0e6",
+        },
+      },
+      fontFamily: {
+        body: ["Raleway"],
+        logo: ["Special Elite"],
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
